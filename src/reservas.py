f228537c689aa6f73b0a2f6caac6efccc3c41eab
santivera111 <santivera111@gmail.com>
2024-09-05 09:43:13 -0500

    CODIGO PROBLEMA 1
    INICIO CON EL CODIGO DEL PROBLEMA 1, CON EL PRIMER PASO DEL PROBLEMA

--- src/reservas.py
+++ src/reservas.py
@@ -2,9 +2,19 @@
 def main():
     #Pseudocodigo
   
+titulo = input("Ingrese su título (Sr. o Sra.): ")
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
 
+print(f"{titulo} {nombre} apellido}, ¡Bienvenido a travel!
 
-    pass # borra esta línea cuando con inicies tu código
+#Seleccion del vuelo
+
+ciudades = {"MEDELLIN" : {"BOGOTA" : 240, "CARTAGENA": 461}, 
+            "BOGOTA":{"MEDELLIN: 240, "CARTAGENA":657}, 
+            "CARTAGENA" : {"MEDELLIN
+
+    
 
 
 if __name__ == "__main__":
