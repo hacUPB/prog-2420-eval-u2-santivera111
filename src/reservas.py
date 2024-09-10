5a8f155829eed110b04f00413502d550c50d50db
santivera111 <santivera111@gmail.com>
2024-09-10 11:29:51 -0500

    Update reservas.py
    Continuó con el código de reservas, actualizando una idea de que el origen es igual al destino, que hacer y adicionar día de la semana, del mes y dejando espacio para continuar con la distancia entre cada opción

--- src/reservas.py
+++ src/reservas.py
@@ -10,11 +10,26 @@ print(f"{titulo} {nombre} apellido}, ¡Bienvenido a travel!
 
 #Seleccion del vuelo
 
-ciudades = {"MEDELLIN" : {"BOGOTA" : 240, "CARTAGENA": 461}, 
-            "BOGOTA":{"MEDELLIN: 240, "CARTAGENA":657}, 
-            "CARTAGENA" : {"MEDELLIN
+origen = input("Escriba su origen en mayúsculas y sin tildes ("MEDELLIN, BOGOTA, CARTAGENA"):
 
-    
+destino = input("Escriba su destino en mayúsculas y sin tildes ("MEDELLIN, BOGOTA, CARTAGENA"):
+
+while origen == destino: 
+ print"El destino no ouede ser el mismo que el origen. Elija un destino diferente ")  
+  destino= input("Escriba su destino en mayusculas y sin tildes: ")
+
+  #fecha 
+
+  dia_semana = input("Escriba el dia de la semana que viajara: (por ejemplo virrnes):")  
+dia_mes= init(input("Escriba el día del mes que viajará (entre 1 y 31): ")
+
+if 1<=dia_mes<= 31:
+ print(f" su viaje se reslizara el dia {dia_mes}")
+else: 
+ print(" siga las instrucciones, Error") 
+ exit()
+
+#Calcular la distancia entre origen y destino
 
 
 if __name__ == "__main__":
