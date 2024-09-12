04df7c0bdfe2aad4683b00d4ad3d2fa8ce6fe135
santivera111 <santivera111@gmail.com>
2024-09-12 09:12:54 -0500

    Update satelite.py
    Realizo inicialmente con un código de base, donde se realizaran varios cambios de aquí en adelante, pero es la idea principal que se seguirá

--- src/satelite.py
+++ src/satelite.py
@@ -1,8 +1,21 @@
 
 def main():
     #Tu código va aquí
-    pass # borra esta línea cuando con inicies tu código
+altitud_inicial = init(input("Escriba altitud inicial en KILOMETROS")
+coeficiente = init(input("Escriba coeficiente de arrastre en decimales pequeños")                     
+altitud_min = init(input("Escriba altitud minima en KILOMETROS") 
+while
+    orbita = 0
+    altitud_perdida = coeficientee * altitud_inicial
+altitud_inicial -=altitud_perdida
+coeficiente += 0.0001
+if: 
+altitud_min = altitud_inicial
+orbita += 1
+else:
+ print("altitud_inicial")
 
+print(f"La cantidad de orbitas realizadas son {orbitas}")
 
 if __name__ == "__main__":
     main()
